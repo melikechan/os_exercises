cb2eff4ddc811d771add48e1bbbad64f7627e8d4
Melike Vurucu <melikechan@tuta.io>
2024-11-09 13:36:41 +0300

    fibonacci: fix fibonacci to run with an user-defined upper bound.

--- threads/fibonacci_pthread.c
+++ threads/fibonacci_pthread.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
-#define MAX 12
+#define THREADS 10
+#define MAX_NUMBER 40
+
+typedef struct
+{
+    int lower_bound;
+    int upper_bound;
+} fibonacci_args;
 
 // Function that calculates the nth Fibonacci number
 int fibonacci(int n)
@@ -14,21 +22,47 @@ int fibonacci(int n)
     return fibonacci(n - 1) + fibonacci(n - 2); // Recursive call
 }
 
-int main(void)
+void *fibonacci_wrapper(void *args)
 {
-    pthread_t threads[MAX]; // Array to hold thread IDs
+    fibonacci_args *f_args = (fibonacci_args *)args;
+    int lower_bound = f_args->lower_bound;
+    int upper_bound = f_args->upper_bound;
 
-    for (int i = 0; i < MAX; i++)
+    int *results = malloc(sizeof(int) * (upper_bound - lower_bound));
+
+    for (int i = lower_bound; i < upper_bound; i++)
     {
-        // First argument -> thread ID, Second argument -> NULL (default attributes), Third argument -> function to be executed by the thread, Fourth argument -> argument to be passed to the function
-        pthread_create(&threads[i], NULL, (void *)fibonacci, (void *)i); // Create a new thread and invoke the fibonacci function with i as the argument
+        results[i - lower_bound] = fibonacci(i);
     }
 
-    for (int i = 0; i < MAX; i++)
+    free(f_args); // Free the allocated memory for arguments
+
+    return (void *)results;
+}
+
+int main(void)
+{
+    pthread_t threads[THREADS];
+
+    for (int i = 0; i < THREADS; i++)
     {
-        int result;
-        pthread_join(threads[i], (void **)&result); // Wait for the thread to finish (first argument thread, second argument the return value of the thread)
-        printf("Fibonacci(%d) = %d\n", i, result);  // Print the result
+        fibonacci_args *args = malloc(sizeof(fibonacci_args));
+        args->lower_bound = i * (MAX_NUMBER / THREADS);
+        args->upper_bound = (i + 1) * (MAX_NUMBER / THREADS);
+
+        // Create a new thread and invoke the fibonacci_wrapper function with args as the argument
+        pthread_create(&threads[i], NULL, fibonacci_wrapper, (void *)args);
+    }
+
+    for (int i = 0; i < THREADS; i++)
+    {
+        int *results;
+        pthread_join(threads[i], (void **)&results);
+
+        for (int j = 0; j < (MAX_NUMBER / THREADS); j++)
+        {
+            printf("Fibonacci(%d) = %d\n", i * (MAX_NUMBER / THREADS) + j, results[j]);
+        }
     }
 
     return 0;
